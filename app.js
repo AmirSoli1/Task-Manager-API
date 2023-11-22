1c0b9f5fb1049c26f4f483a3c07f9b5589862d9a
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-22 12:26:08 +0200

    update server to run after db is connected

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-require("./db/connect");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-
 const tasksRouter = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
+app.use(express.json());
 app.use("/api/v1/tasks", tasksRouter);
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}....`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}....`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-const connectionString =
-  "mongodb+srv://Maon:Tnhr123789!@cluster0.nexcvea.mongodb.net/Task-Manager?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("CONNECTED TO THE DB..."))
-  .catch((err) => console.log(err));
+const connectDB = (url) => {
+  return mongoose.connect(url);
+};
+
+module.exports = connectDB;
