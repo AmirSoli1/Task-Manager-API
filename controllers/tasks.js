d5afa3a1b739f373a3dcf47cb3cbbb0e1dd70ac3
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-22 14:00:38 +0200

    add the option to create tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,27 @@
+const Task = require('../models/Task');
 const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+  res.send('get all tasks');
 };
 
-const createTask = (req, res) => {
-  res.send("Task created.");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 const getSingleTask = (req, res) => {
-  res.send("get single task");
+  res.send('get single task');
 };
 
 const updtaeTask = (req, res) => {
-  res.send("Task updated");
+  res.send('Task updated');
 };
 
 const deleteTask = (req, res) => {
-  res.send("Task deleted.");
+  res.send('Task deleted.');
 };
 
 module.exports = {
