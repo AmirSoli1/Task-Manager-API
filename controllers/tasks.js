35e4e0978a8804ac7cf8a370f801b0b1d6ecfd6c
Amir Solimany <amirsoli@post.bgu.ac.il>
2023-11-22 14:37:06 +0200

    Add the option to search tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,11 @@
 const Task = require('../models/Task');
-const getAllTasks = (req, res) => {
-  res.send('get all tasks');
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 const createTask = async (req, res) => {
@@ -12,8 +17,16 @@ const createTask = async (req, res) => {
   }
 };
 
-const getSingleTask = (req, res) => {
-  res.send('get single task');
+const getSingleTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    if (!task)
+      return res.status(404).json({ msg: `No task with id: ${id} exists` });
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
 const updtaeTask = (req, res) => {
